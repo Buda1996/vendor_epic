028b71c4e8d98e53f742382497ed9b56c62db7a1
Ateekujjawal <ateekujjawal@gmail.com>
2015-01-16 16:22:48 +0530

    Don't use cm build targets

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,1 @@
-for combo in $(curl -s https://raw.githubusercontent.com/Epic-OS/hudson/master/epic-build-targets | sed -e 's/#.*$//' | grep E-L5 | awk {'print $1'})
-do
-    add_lunch_combo $combo
-done
+add_lunch_combo cm_sprout-userdebug
